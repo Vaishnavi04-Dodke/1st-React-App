5ca32daf246b889978e15f9595ea2a65daa23fc1
Vaishnavi Dodke <vaishnavidodke55@gmail.com>
2024-01-17 13:10:48 +0530

    Update Bussines and Testimonal components

--- src/Components/Bussiness/Bussines.jsx
+++ src/Components/Bussiness/Bussines.jsx
@@ -1,60 +1,34 @@
+
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import image from "./../../Images/3.png";
-import "./Bussines.css";
 import Image from  "../../Images/increase.png";
+import "./Bussines.css";
+import image from "./../../Images/3.png";
 
 function Bussines() {
-//   const columns = [
-//     {
-//       Heading: "We'll help perfect your business grow !",
-//       Content: [
-//         <p key={1}>
-       
-//           "Experience the lightning-fast transformation of your business with
-//           Connectana's revolutionary website solutions - go global and conquer
-//           the digital world in just 60 minutes!"
-//         </p>,
-//       ],
-//     },
-//     {
-//       Heading: "",
-//       Content: [
-//         <img key={1} src={image} alt="About Us" className="img-fluid" />,
-//       ],
-//     },
-//   ];
-
   return (
-    <>
-    <section className="about-us-continue" >
+    <section className="about-us-continue">
       <Container fluid className="container-md container-lg p-3 ps-md-5 ps-lg-5 pe-md-5 pe-lg-5">
-      <Row className="row align-items-center p-0 m-0 pt-2">
-          {/* {columns.map((column, index) => (
-            <Col key={index} className="Bussiness-content p-5 m-5">
-              <h1>{column.Heading}</h1>
-              {column.Content}
-            </Col>
-          ))} */}
-          <Col className="col-md-6 about-contain p-4 mt-5 mt-md-0 mt-lg-0">
-          {/* <div className="content p-4 " > */}
-            <img src={Image} alt="Business Growth" width={"10%"} className="m-3 border rounded p-2"></img>
-            <h2 className="fw-bolder" >"We'll help perfect</h2>  
-            <h2 className="fw-bolder">your business grow !"</h2>
-            <p className="fs-6 text-justify">"Experience the lightning-fast transformation of your business with
-          Connectana's revolutionary website solutions - go global and conquer
-          the digital world in just 60 minutes!"</p>
-        {/* </div> */}
+        <Row className="align-items-center p-0 m-0 pt-2 flex-md-row flex-lg-row flex-column-reverse">
+          <Col xs={12} md={6} className="about-contain p-4 mt-5 mt-md-0 mt-lg-0">
+            <img src={Image} alt="Business Growth" className="img-fluid m-3 border rounded p-2" />
+            <h2 className="fw-bolder">"We'll help perfect your business grow!"</h2>
+            <p className="fs-6 text-justify">Experience the lightning-fast transformation of your business with Connectana's revolutionary website solutions - go global and conquer the digital world in just 60 minutes!</p>
           </Col>
-          <Col className="col-md-6 p-0 m-0 text-md-end text-lg-end">
-          <img src={image} alt="Business Growth Illustration" width={"100%"} />
-
+          <Col xs={12} md={6} className="p-0 m-0 text-md-end text-lg-end">
+            <img src={image} alt="Business Growth Illustration" className="img-fluid" />
           </Col>
         </Row>
       </Container>
-      </section>
-    </>
+    </section>
   );
 }
 
 export default Bussines;
+
+
+
+
+
+
+
--- src/Components/Testimonal/Testominal.jsx
+++ src/Components/Testimonal/Testominal.jsx
@@ -1,59 +1,55 @@
+
 import React from "react";
-import "./Testimonal.css";
 import { Col, Container, Row } from "react-bootstrap";
 import Image from "../../Images/first.png";
 import Image1 from "../../Images/second.png";
 import Image3 from "../../Images/third.png";
-// function column data
 
-    function Card({ text }) {
-      return (
-        <Col className="cards col-md-4 p-4 text-center">
-          <img src={text.image} alt={`Testimonial by ${text.owner}`} className="rounded-circle mb-4" />
-          <p className="p">{text.content}</p>
-          <small className="text-color-green fw-bold">{text.owner}</small>
-        </Col>
-      );
-    }
-    
-
-
-// function create client data
-
-function Client({items ,index}){
-    return(
-        <Col key={index} className="mt-lg-0 mt-4">
-        <h1 className="text-color-green">{items.heading}</h1>
-        <p className="fs-6  mb-5">{items.content}</p>
-      </Col>
-    )
+function Card({ text }) {
+  return (
+    <Col xs={12} md={4} lg={4} className="cards col-md-4 p-4 text-center" style={{ backgroundColor: "#f4f4f4", borderRadius: "20px", margin: "10px" }}>
+      <img src={text.image} alt={`Testimonial by ${text.owner}`} className="rounded-circle mb-4" />
+      <p className="p">{text.content}</p>
+      <small className="text-color-green fw-bold">{text.owner}</small>
+    </Col>
+  );
 }
+
+function Client({ items }) {
+  return (
+    <Col xs={12} className="mt-lg-0 mt-4">
+      <h1 className="text-color-green">{items.heading}</h1>
+      <p className="fs-6 mb-5">{items.content}</p>
+    </Col>
+  );
+}
+
 function Testimonial() {
   const client = [
     {
       heading: "Client Testimonials",
       content: "We connect your Business to your customers",
-     
     },
   ];
+
   const column = [
     {
       image: Image,
       content:
         "Connectana not only delivered a stunning website for my business, but they did it in record time! I couldn't believe how quickly I had a professional online presence. Highly recommend their services!",
-        owner:"- Catherine Farai, Sambasi "
+      owner: "- Catherine Farai, Sambasi ",
     },
     {
       image: Image1,
       content:
         "I was amazed by how easy it was to get my website up and running with Connectana. In just under an hour, I had a beautiful storefront that showcased my products perfectly. It's been a game-changer for my online sales!",
-        owner:"- Tawanda Mande, Golstra "
+      owner: "- Tawanda Mande, Golstra ",
     },
     {
       image: Image3,
       content:
         "Connectana's fast and efficient website solutions helped me establish a strong digital presence for my consultancy services. Within minutes, I had a sleek and professional website that attracted clients from all over the world. I couldn't be happier!",
-    owner:"- Neil Tran, Gaviez Express "
+      owner: "- Neil Tran, Gaviez Express ",
     },
   ];
 
@@ -61,12 +57,10 @@ function Testimonial() {
     <Container className="container-md container-lg p-3 ps-md-5 ps-lg-5 pe-md-5 pe-lg-5 pb-md-5 pb-lg-5">
       <Row className="text-center">
         {client.map((item, index) => (
-       
-<Client items={item} key={index}/>
-       
-       ) )}
+          <Client items={item} key={index} />
+        ))}
       </Row>
-      <Row className="d-grid gap-1 d-flex flex-wrap flex-md-nowrap flex-lg-nowrap p-0 m-0 small-cards mt-5 mb-5 mb-lg-0 mb-md-0 ">
+      <Row className="d-flex justify-content-center align-items-center p-0 m-0 small-cards mt-5 mb-5 mb-lg-0 mb-md-0 ">
         {column.map((item, index) => (
           <Card text={item} key={index} />
         ))}
@@ -76,3 +70,4 @@ function Testimonial() {
 }
 
 export default Testimonial;
+
